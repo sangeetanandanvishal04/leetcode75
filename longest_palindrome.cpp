1df21d60c5f3aaf272a1e9b5154418b8a0a00b19
sangeetanandanvishal04 <sangeetanandanvishalsingh04@gmail.com>
2025-06-05 15:59:49 +0530

    longest palindromic substring done

---
+++ longest_palindrome.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private: 
+    string expandAroundCenter(string s, int l, int r, int n){
+        while(l>=0 && r<n && s[l] == s[r]){
+            l--;
+            r++;
+        }
+
+        return s.substr(l+1, r-l-1);
+    }    
+public:
+    string longestPalindrome(string s) {
+        int n = s.length();
+        string result = "";
+
+        for(int i=0; i<n; i++){
+            string even = expandAroundCenter(s, i, i+1, n); 
+            if(even.length() > result.length()){
+                result = even;
+            }
+
+            string odd = expandAroundCenter(s, i, i, n);
+            if(odd.length() > result.length()){
+                result = odd;
+            }
+        }
+
+        return result;
+    }
+};
+
+int main(){
+    Solution sol;
+    string s = "babad";
+
+    string result = sol.longestPalindrome(s);
+    cout << result << endl;
+    return 0;
+}
