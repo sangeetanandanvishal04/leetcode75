774b0b0b154655eed16f0bca87516ca9dfe404cf
sangeetanandanvishal04 <sangeetanandanvishalsingh04@gmail.com>
2025-06-05 13:04:47 +0530

    min window substring done

---
+++ min_window_substring.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        vector<int> vec(128, 0);
+        int n = s.length();
+        int m = t.length();
+
+        for(char ch: t){
+            vec[ch]++;
+        }
+
+        int l=0, r=0, cnt=0, minLen=INT_MAX;
+        int startIdx = -1;
+
+        while(r < n){
+            if(vec[s[r]] > 0){
+                cnt++;
+            }
+            vec[s[r]]--;
+
+            while(cnt == m){
+                if(r-l+1 < minLen){
+                    startIdx = l;
+                    minLen = r-l+1;
+                }
+
+                vec[s[l]]++;
+                if(vec[s[l]] > 0){
+                    cnt--;
+                }
+
+                l++;
+            }
+        
+            r++;
+        }
+
+        return startIdx == -1? "": s.substr(startIdx, minLen);
+    }
+}; 
+
+int main(){
+    Solution sol;
+    string s = "ADOBECODEBANC";
+    string t = "ABC";
+
+    string result = sol.minWindow(s, t);
+    cout << result << endl;
+    return 0;
+}
